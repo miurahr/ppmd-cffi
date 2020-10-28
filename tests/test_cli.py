b2c5d2e143406fa2da1e406d0c67f969e9042c40
Hiroshi Miura <miurahr@linux.com>
2020-10-29 07:49:15 +0900

    Add more tests
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+import binascii
+import hashlib
 import os
 import shutil
 
@@ -35,3 +37,15 @@ def test_cli_extract(tmp_path):
     #
     result = extracted.open('rb').read()
     assert result == source
+
+
+def test_cli_compress(tmp_path):
+    arcfile = os.path.join(testdata_path, "10000SalesRecords.csv")
+    shutil.copy(arcfile, tmp_path.joinpath('10000SalesRecords.csv'))
+    target = str(tmp_path.joinpath('10000SalesRecords.csv'))
+    compressed = tmp_path.joinpath('10000SalesRecords.csv.ppmd')
+    main([target])
+    #
+    m = hashlib.sha256()
+    m.update(compressed.open('rb').read())
+    assert m.digest() == binascii.unhexlify('ecc320d73dc83ea73510dbeaec6ee6bf0a33bb5e84177465f1c544452a1b6041')
