1a686bcd264a469e45245642faca19d16f27faea
Hiroshi Miura <miurahr@linux.com>
2021-02-05 23:18:21 +0900

    PEP8
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/ppmd/__init__.py
+++ src/ppmd/__init__.py
@@ -26,11 +26,13 @@ from datetime import datetime, timezone
 from typing import Any, BinaryIO, Optional
 
 try:
-    from importlib.metadata import PackageNotFoundError, version
+    from importlib.metadata import PackageNotFoundError
+    from importlib.metadata import version
 except ImportError:
-    from importlib_metadata import PackageNotFoundError, version  # type: ignore  # noqa
+    from importlib_metadata import PackageNotFoundError  # type: ignore  # noqa
+    from importlib_metadata import version  # type: ignore  # noqa
 
-__copyright__ = 'Copyright (C) 2020 Hiroshi Miura'
+__copyright__ = 'Copyright (C) 2020,2021 Hiroshi Miura'
 
 try:
     __version__ = version(__name__)
