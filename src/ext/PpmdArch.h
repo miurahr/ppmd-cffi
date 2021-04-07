5e08767a7ef9b00a9cd101ded9807c65500752c2
Hiroshi Miura <miurahr@linux.com>
2021-04-07 09:30:13 +0900

    Diet C codes
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/ext/PpmdArch.h
+++ src/ext/PpmdArch.h
@@ -72,33 +72,9 @@ struct ISzAlloc
 #define ISzAlloc_Alloc(p, size) (p)->Alloc(size)
 #define ISzAlloc_Free(p, a) (p)->Free(a)
 
-#if  defined(_M_X64) \
-  || defined(_M_AMD64) \
-  || defined(__x86_64__) \
-  || defined(__AMD64__) \
-  || defined(__amd64__)
-#define MY_CPU_AMD64
-  #define MY_CPU_64BIT
-#endif
-
-
 #if  defined(_M_IX86) \
-  || defined(__i386__)
-#define MY_CPU_X86
-  #define MY_CPU_32BIT
-#endif
-
-
-#if  defined(_M_ARM64) \
-  || defined(__AARCH64EL__) \
-  || defined(__AARCH64EB__) \
-  || defined(__aarch64__)
-#define MY_CPU_ARM64
-  #define MY_CPU_64BIT
-#endif
-
-
-#if  defined(_M_ARM) \
+  || defined(__i386__) \
+  || defined(_M_ARM) \
   || defined(_M_ARM_NT) \
   || defined(_M_ARMT) \
   || defined(__arm__) \
@@ -106,41 +82,12 @@ struct ISzAlloc
   || defined(__ARMEL__) \
   || defined(__ARMEB__) \
   || defined(__THUMBEL__) \
-  || defined(__THUMBEB__)
-#define MY_CPU_ARM
-  #define MY_CPU_32BIT
-#endif
-
-
-#if  defined(_M_IA64) \
-  || defined(__ia64__)
-#define MY_CPU_IA64
-  #define MY_CPU_64BIT
-#endif
-
-
-#if  defined(__mips64) \
-  || defined(__mips64__) \
-  || (defined(__mips) && (__mips == 64 || __mips == 4 || __mips == 3))
-#define MY_CPU_64BIT
-#elif defined(__mips__)
-/* #define MY_CPU_32BIT */
-#endif
-
-
-#if  defined(__ppc64__) \
-  || defined(__powerpc64__)
-#define MY_CPU_64BIT
-#elif defined(__ppc__) \
-  || defined(__powerpc__)
-#define MY_CPU_32BIT
-#endif
-
-
-#if  defined(__sparc64__)
-#define MY_CPU_64BIT
-#elif defined(__sparc__)
-/* #define MY_CPU_32BIT */
+  || defined(__THUMBEB__) \
+  || defined(__mips__) \
+  || defined(__ppc__) \
+  || defined(__powerpc__) \
+  || defined(__sparc__)
+  #define PPMD_32BIT
 #endif
 
 EXTERN_C_END
